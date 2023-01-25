4e1bd8d51e05bf276826980d041916f0d77a1a07
justremember <nothingpersonal.qwerty@gmail.com>
2023-01-25 11:37:11 +0800

    make base layout

---
+++ src/components/KanaInputTrainer.js
@@ -0,0 +1,10 @@
+export default function KanaInputTrainer() {
+  return (
+    <div className='kana-input-trainer'>
+      <div className='kana-question'>
+        ろ
+      </div>
+      <input type='text' className='form-control' placeholder='Type kana here' />
+    </div>
+  );
+}
