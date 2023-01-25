db76d106a782d142f76ded5cf80c6d3779669fdb
justremember <nothingpersonal.qwerty@gmail.com>
2023-01-25 11:53:50 +0800

    include next-questions to layout

--- src/components/KanaInputTrainer.js
+++ src/components/KanaInputTrainer.js
@@ -1,8 +1,16 @@
 export default function KanaInputTrainer() {
   return (
     <div className='kana-input-trainer'>
-      <div className='kana-question'>
-        ろ
+      <div className='kana-prompt d-flex justify-content-center align-items-center'>
+        <div className='kana-ghost'>
+          {'空気' /* this is a placeholder so the next-question will be centered */}
+        </div>
+        <div className='kana-next-question'>
+          ろ
+        </div>
+        <div className='kana-questions-after'>
+          ふあ
+        </div>
       </div>
       <input type='text' className='form-control' placeholder='Type kana here' />
     </div>
