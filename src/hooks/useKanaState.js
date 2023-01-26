12cb3aa1b630612835416012bfe5bdeb3c721682
justremember <nothingpersonal.qwerty@gmail.com>
2023-01-26 14:15:09 +0800

    revamp srs algorithm, introduce priority

--- src/hooks/useKanaState.js
+++ src/hooks/useKanaState.js
@@ -5,25 +5,21 @@ export const LOOKAHEAD = 1;
 
 const allKanasInOrder = 'ちとしはきくまのりれけたていすかんなにらせむろぬふあうえおやゆよわほへつさそひこみもねるめ';
 const kanasArray = allKanasInOrder.split('');
+const initialPriority = kanasArray.reduce((o, key, i) => ({ ...o, [key]: i * 5}), {});
 
 const initialState = {
   ease: kanasArray.reduce((o, key) => ({ ...o, [key]: 1}), {}),
-  queue: kanasArray
+  queue: kanasArray,
+  priority: initialPriority
 }
 
 // reposition the first kana in the queue depending on its ease
-function repositionKana(queue, ease) {
-  let newPos;
-  if (ease < queue.length * 3/4) {
-	newPos = LOOKAHEAD + ease + Math.floor(Math.random() * 2);
-  } else {
-	// place kana in a random position in the 2nd half of the queue
-	newPos = LOOKAHEAD + Math.ceil(queue.length/2) + Math.floor(Math.random() * (queue.length/2 - LOOKAHEAD));
-  }
-  console.log(newPos);
-  const firstHalf = queue.slice(1, newPos);
-  const secondHalf = queue.slice(newPos);
-  return firstHalf.concat([queue[0]], secondHalf);
+function repositionKana({ queue, ease, priority }) {
+  const kana = queue[0];
+  priority[kana] += ease[kana] + Math.floor(Math.random() * ease[kana] / 2);
+  const backlogToSort = [kana, ...queue.slice(1 + LOOKAHEAD)];
+  const sortedBacklog = backlogToSort.sort((k1, k2) => priority[k1] - priority[k2]);
+  return queue.slice(1, 1 + LOOKAHEAD).concat(sortedBacklog);
 }
 
 export default function useKanaState() {
@@ -31,7 +27,7 @@ export default function useKanaState() {
     const prompt = state.queue[0];
     switch (action.type) {
       case 'correct': {
-        const newQueue = repositionKana(state.queue, state.ease[prompt]);
+        const newQueue = repositionKana(state);
         const newEaseVal = state.ease[prompt] * 2;
         const newEase = { ...state.ease, [prompt]: newEaseVal };
         const newState = {
@@ -43,9 +39,11 @@ export default function useKanaState() {
         return newState;
       }
       case 'wrong': {
-        const newPromptEaseVal = Math.max(1, Math.floor(state.ease[prompt] / 4));
-        const newWrongAnsEaseVal = Math.max(1, Math.floor(state.ease[action.ans] / 4));
-        const newEase = { ...state.ease, [prompt]: newPromptEaseVal, [action.ans]: newWrongAnsEaseVal };
+        const newEase = {
+          ...state.ease,
+          [prompt]: Math.max(1, Math.floor(state.ease[prompt] / 8)),
+          [action.ans]: Math.max(1, Math.floor(state.ease[action.ans] / 1))
+        };
         const newState = {
           ...state,
           ease: newEase,
@@ -54,7 +52,10 @@ export default function useKanaState() {
         return newState;
       }
       case 'loadState': {
-        return action.state;
+        return {
+          ...initialState,
+          ...action.state
+        };
       }
       default:
         console.log('unrecognized action type:', action.type);
@@ -66,48 +67,3 @@ export default function useKanaState() {
 
   return [state, dispatch, LOOKAHEAD];
 }
-/*
-export default function useKanaState() {
-  const [savedState, saveState] = useLocalStorageState(
-    'kanaState',
-    { defaultValue: initialState },
-  );
-
-  const reducer = (state, action) => {
-    const prompt = state.queue[0];
-    switch (action.type) {
-      case 'correct': {
-        const newEaseVal = state.ease[prompt] * 2;
-        const newEase = { ...state.ease, [prompt]: newEaseVal };
-        const newState = {
-          ...state,
-          ease: newEase,
-          queue: repositionKana(state.queue, newEase[prompt])
-        };
-        console.log({ newState });
-        saveState(newState);
-        return newState;
-      }
-      case 'wrong': {
-        const newPromptEaseVal = Math.max(1, Math.floor(state.ease[prompt] / 2));
-        const newWrongAnsEaseVal = Math.max(1, Math.floor(state.ease[action.ans] / 2));
-        const newEase = { ...state.ease, [prompt]: newPromptEaseVal, [action.ans]: newWrongAnsEaseVal };
-        const newState = {
-          ...state,
-          ease: newEase,
-        };
-        console.log({ newState });
-        saveState(newState);
-        return newState;
-      }
-      default:
-        console.log('unrecognized action type:', action.type);
-        return state;
-    }
-  }
-
-  const [state, dispatch] = useReducer(reducer, savedState);
-
-  return [state, dispatch, LOOKAHEAD];
-}
-  */
